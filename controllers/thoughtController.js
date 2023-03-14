3805d1e55a9a435cf34c3ee3ec5ccfa08dccbea5
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-13 21:43:52 -0400

    added function getThoughtById to thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,9 +7,9 @@ module.exports = {
         Thought.create(req.body)
         .then((thoughtData) => {
             return User.findOneAndUpdate(
-                {_id:req.body.userID},
-                {$push:{thoughts:data._id}},
-                {new:true}
+                { _id: req.body.userID },
+                { $push: {thoughts:data._id} },
+                { new:true }
             )
         })
         .then(userData => res.json(userData))
@@ -20,11 +20,22 @@ module.exports = {
         .then((thought) => response.json(thought))
         .catch((err) => res.status(500).json(err));
     },
-    getThoughtById(req,res) {
-
+    getThoughtById({ params },res) {
+        Thought.findOne({ _id: params.id })
+        .then((thoughtData) => {
+            if(!thoughtData) {
+                res.status(404).json({ message: 'Thought not found; invalid ID'});
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     updateThoughtById(req,res) {
-
+        
     },
     addReaction(req,res) {
 
