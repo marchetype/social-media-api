065663eae0b370013cb8378a7ddbf29dba31184f
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-13 22:22:23 -0400

    started work on userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,25 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    
+    newUser(req,res) {
+
+    },
+    getAllUsers(req,res) {
+        
+    },
+    getUserById(req,res) {
+
+    },
+    updateUserById(req,res) {
+
+    },
+    deleteUserById(req,res) {
+
+    },
+    addFriendById(req,res) {
+
+    },
+    removeFriendById(req,res) {
+        
+    }
 }
