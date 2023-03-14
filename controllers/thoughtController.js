2df519e8634439259a70aa07b23faef4b99d4d2c
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-13 21:34:46 -0400

    updated thoughtController.js, added functions newThought and getAllThoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,5 +1,35 @@
+const { response } = require('express');
 const { Thought, User } = require('../models');
+const { populate } = require('../models/User');
 
 module.exports = {
-    
+    newThought(req,res) {
+        Thought.create(req.body)
+        .then((thoughtData) => {
+            return User.findOneAndUpdate(
+                {_id:req.body.userID},
+                {$push:{thoughts:data._id}},
+                {new:true}
+            )
+        })
+        .then(userData => res.json(userData))
+        .catch((err) => res.status(500).json(err));
+    },
+    getAllThoughts(req,res) {
+        Thought.find()
+        .then((thought) => response.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
+    getThoughtById(req,res) {
+
+    },
+    updateThoughtById(req,res) {
+
+    },
+    addReaction(req,res) {
+
+    },
+    removeReactionById(req,res) {
+
+    }
 }
