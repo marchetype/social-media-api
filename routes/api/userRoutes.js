f9074d93bc20fac260edd28ed0ece7195ecf49b9
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-13 20:14:39 -0400

    Finished userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,4 +12,9 @@ const {
 
 //establish GET and POST routes for /api/users
 router.route('/').get(getAllUsers).post(newUser);
+//establish GET, PUT, and DELETE for /api/users/:id
+router.route('/:id').get(getUserById).put(updateUserById).delete(deleteUserById);
+//establish POST and DELETE routes for api/users/:userId/friends/:friendId
+router.route('/:id/friends/:friendsId').post(addFriendById).delete(removeFriendById);
 
+module.exports = router;
